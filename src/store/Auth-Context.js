0ccfb7dff06aabbb90aa000903ee29fea1fd0c8a
Al Arafat Siddique <arafatdipto@gmail.com>
2022-09-21 13:12:41 +0600

    Auto Log out implemented

--- src/store/Auth-Context.js
+++ src/store/Auth-Context.js
@@ -6,17 +6,31 @@ const AuthContext = React.createContext({
     logout: ()=>{}
 });
 
+const calculateRemainingTime= (expirationTime)=>{
+    const currentTime = new Date().getTime();
+    const adjExpirationTime= new Date(expirationTime).getTime();
+
+    const remainingDuration = adjExpirationTime - currentTime;  
+    return remainingDuration;
+}
+
 export const AuthContextProvider = (props)=>{
     const initialToken = localStorage.getItem('token');
     const [token, setToken]= useState(initialToken);
     const isLoggedIn = !!token;
-    const logInHandler=(token)=>{
-        localStorage.setItem('token',token);
-        setToken(token);
-    }
+
     const logOutHandler= ()=>{
         localStorage.removeItem('token');
         setToken(null);
+    }
+    
+    const logInHandler=(token, expirationTime)=>{
+
+        localStorage.setItem('token',token);
+        setToken(token);
+
+        const remainingTime = calculateRemainingTime(expirationTime);
+        setTimeout(logOutHandler, remainingTime);
     }
 
     const contextValue={
